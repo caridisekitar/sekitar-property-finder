9225bb3f88a52dd8ebea6ee0c34aac64b2df898c
iwakbhetok <abdul.createit@gmail.com>
2026-04-15 11:14:23 +0700

    fixing logo on safari

--- components/icons/SekitarLogo.tsx
+++ components/icons/SekitarLogo.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const SekitarLogo: React.FC<{ className?: string }> = ({ className }) => (
-  <img src="/images/LogoHeader.svg" alt="Sekitar Logo" width={50} height={50} className={className} />
+  <img src="/images/logo-header-sekitar.png" alt="Sekitar Logo" className={className} />
 );
 
 export default SekitarLogo;
